212d8117c06d49f6a5772636606ed8ff9f6c9f98
zhaojipeng <zhaojipeng@haodai.com>
2016-08-19 16:10:13 +0800

    config

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhaojipeng
+ * Date: 16/8/19
+ * Time: 14:54
+ */
+
+$GLOBALS = [
+    'curl_common_params' => [
+        'login_token' => '17625,ec5882d6e17217d122d9d3c17315f1c6',
+        'format' => 'json',
+        'lang' => 'cn',
+        'error_on_empty' => 'yes',
+    ],
+    'domain' => 'fhqfkeeq.com',
+];

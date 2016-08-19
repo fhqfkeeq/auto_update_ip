2d6bf190aba59915cdbd8b82988c7336254d5f49
zhaojipeng <zhaojipeng@haodai.com>
2016-08-19 16:14:00 +0800

    config

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhaojipeng
+ * Date: 16/8/19
+ * Time: 14:54
+ */
+
+$GLOBALS = [
+    'curl_common_params' => [
+        'login_token' => 'xxx,xxx',
+        'format' => 'json',
+        'lang' => 'cn',
+        'error_on_empty' => 'yes',
+    ],
+    'domain' => 'xxx.com',
+];
